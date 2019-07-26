eadc2e4e0f625e01669762e306f4f13164d8daa4
mghlm <mail@magnusholm.com>
2019-07-26 14:06:50 +0100

    Initial Commit

---
+++ CurrencyRates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyRates
+//
+//  Created by Magnus Holm on 26/07/2019.
+//  Copyright © 2019 Magnus Holm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
